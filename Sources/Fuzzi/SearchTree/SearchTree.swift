b8f1ac1d04bada5e9652bad9b9d5091df79ddccc
Mathias Quintero <me@quintero.io>
2020-05-26 22:48:19 +0200

    Moving files around

--- Sources/Fuzzi/SearchTree/SearchTree.swift
+++ Sources/Fuzzi/SearchTree/SearchTree.swift
@@ -13,6 +13,17 @@ extension SearchTree {
     public func search(query: String,
                        maxDistance: Int = 4,
                        relevantAfter: Double = 0.6,
+                       options: SearchOptions = .standard) -> [SearchResult<ID>] {
+
+        return search(query: query,
+                      maxDistance: maxDistance,
+                      relevantAfter: relevantAfter,
+                      options: options)
+    }
+
+    public func search(query: String,
+                       maxDistance: Int = 4,
+                       relevantAfter: Double = 0.6,
                        options: SearchOptions = .standard) -> [ID] {
 
         return search(query: query,
